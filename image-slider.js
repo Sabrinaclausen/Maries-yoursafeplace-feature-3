7652c77572d4a2e46660ff65b9912c660af413fd
Rosa2401 <Bosa2401@gmail.com>
2025-12-09 20:08:34 +0100

    Image Slider
    Image Slider

---
+++ image-slider.js
@@ -0,0 +1,38 @@
+let isDown = false;
+let startX;
+let scrollLeft;
+const slider = document.querySelector('.slider-event-images');
+
+const end = () => {
+	isDown = false;
+  slider.classList.remove('active');
+}
+
+const start = (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = (e.pageX || e.touches[0].pageX) - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;	
+}
+
+const move = (e) => {
+	if(!isDown) return;
+
+  e.preventDefault();
+  const x = (e.pageX || e.touches[0].pageX) - slider.offsetLeft;
+  const dist = (x - startX);
+  slider.scrollLeft = scrollLeft - dist;
+}
+
+(() => {
+    slider.addEventListener('mousedown', start);
+    slider.addEventListener('touchstart', start, { passive: false });
+
+    slider.addEventListener('mousemove', move);
+    slider.addEventListener('touchmove', move, { passive: false });
+
+    slider.addEventListener('mouseleave', end);
+
+    document.addEventListener('mouseup', end);
+    document.addEventListener('touchend', end);
+})();
